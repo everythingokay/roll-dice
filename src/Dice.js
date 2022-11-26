88c76963b6a16f8ca7d09c4150fb57b1101e2999
Alison Feldish <alison.feldish@gmail.com>
2022-11-26 18:23:08 -0500

    animation & styling

--- src/Dice.js
+++ src/Dice.js
@@ -4,7 +4,8 @@ import './Dice.css';
 class Dice extends Component {
     render() {
         return (
-            <i className={`dice fas fa-dice-${this.props.face}`}></i>
+            <i className={`dice fas fa-dice-${this.props.face} ${this.props.rolling && "rolling"}`}>
+            </i>
         )
     }
 };
--- src/Roll.js
+++ src/Roll.js
@@ -1,3 +1,4 @@
+import { toHaveStyle } from '@testing-library/jest-dom/dist/matchers';
 import React, { Component } from 'react';
 import Dice from './Dice';
 import './Roll.css';
@@ -8,22 +9,27 @@ class Roll extends Component {
     };
     constructor(props) {
         super(props);
-        this.state = { die1: "one", die2: "one" };
+        this.state = { die1: "one", die2: "one", rolling: false };
         this.roll = this.roll.bind(this);
     };
     roll() {
         const newDie1 = this.props.sides[Math.floor(Math.random() * this.props.sides.length)];
         const newDie2 = this.props.sides[Math.floor(Math.random() * this.props.sides.length)];
-        this.setState({ die1: newDie1, die2: newDie2 });
+        this.setState({ die1: newDie1, die2: newDie2, rolling: true });
+        setTimeout(() => {
+            this.setState({ rolling: false })
+        }, 1000);
     };
     render() {
         return (
             <div className="roll">
                 <div className="roll-container">
-                    <Dice face={this.state.die1} />
-                    <Dice face={this.state.die2} />
+                    <Dice face={this.state.die1} rolling={this.state.rolling} />
+                    <Dice face={this.state.die2} rolling={this.state.rolling} />
                 </div>
-                <button onClick={this.roll}>Roll Dice!</button>
+                <button onClick={this.roll} disabled={this.state.rolling}>
+                    {this.state.rolling ? "Rolling..." : "Roll Dice!"}
+                </button>
             </div>
         )
     }
