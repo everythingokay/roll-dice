ac7128753d0fd8d5f87e439f97e2fc0dc9da092a
Alison Feldish <alison.feldish@gmail.com>
2022-11-26 17:41:48 -0500

    set state & random logic

--- src/Roll.js
+++ src/Roll.js
@@ -8,12 +8,19 @@ class Roll extends Component {
     constructor(props) {
         super(props);
         this.state = { die1: "one", die2: "one" };
+        this.roll = this.roll.bind(this);
+    };
+    roll() {
+        const newDie1 = this.props.sides[Math.floor(Math.random() * this.props.sides.length)];
+        const newDie2 = this.props.sides[Math.floor(Math.random() * this.props.sides.length)];
+        this.setState({ die1: newDie1, die2: newDie2 });
     };
     render() {
         return (
             <div>
                 <Dice face={this.state.die1} />
                 <Dice face={this.state.die2} />
+                <button onClick={this.roll}>Roll Dice!</button>
             </div>
         )
     }
